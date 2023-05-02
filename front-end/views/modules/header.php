f1c136521430d4c634355bb6023d927cdbcb0cdb
Mauricio Astudillo <gatoalternativo@gmail.com>
2023-05-02 18:29:08 -0500

    Commit 2-5-2023 begin header

---
+++ front-end/views/modules/header.php
@@ -0,0 +1,141 @@
+<div class="container-fluid topBar" id="top">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 initial-banner">
+                <h5>10% Off, using code "ZOE10MAYO"</h5>
+            </div>
+        </div>
+        <div class="row mainHeader">
+            <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+                <ul class="float-start">
+                    <li>
+                        <a href="#">
+                            <i class="bi bi-facebook socialNetwork" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="bi bi-instagram socialNetwork" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 register">
+                <ul>
+                    <li><a href="#signinModal">Log In</a></li>
+                    <li>|</li>
+                    <li><a href="#loginModal">Sign In</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- HEADER -->
+
+<header class="container-fluid">
+    <div class="container">
+        <div class="row" id="header">
+            <!-- LOGO -->
+            <div class="col-lg3 col-md-3 col-sm-2 col-xs-12" id="logo">
+                <a href="#">
+                    <img src="http://localhost/e-commerce/back-end/views/img/template/logo.png" class="img-responsive">
+                </a>
+            </div>
+            <!-- Categories -->
+            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+                
+                <!-- Categories button -->
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategories">
+                    <p>Categories
+                        <span class="float-right">
+                            <i class="bi bi-list"></i>
+                        </span>
+                    </p>
+                </div>
+                <!-- Search -->
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 input-group" id="search">
+                    <input type="search" name="search" class="form-control" placeholder="Search">
+                    <span class="input-group-btn">
+                        <a href="#">
+                            <button class="btn btn-default backColor" type="submit">
+                                <i class="bi bi-search"></i>
+                            </button>
+                        </a>
+                    </span>
+                </div>
+            </div>
+            <!-- Shoping bag -->
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+                <a href="#">
+                    <button class="btn btn-default float-left backcolor">
+                        <i class="bi bi-bag" aria-hidden="true"></i>
+                    </button>
+                </a>
+                <p>Shopping Bag <span class="bagQuantity"> <br> USD $ <span class="totalBag">100.00</span></span></p>
+            </div>
+
+        </div>
+
+
+
+
+        <!-- Categories -->
+        <div class="col-12 backColor" id="categories">
+
+
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategories">Lorem Ipsum</a>
+                </h4>
+                <hr>
+                <ul>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategories">Lorem Ipsum</a>
+                </h4>
+                <hr>
+                <ul>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategories">Lorem Ipsum</a>
+                </h4>
+                <hr>
+                <ul>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategories">Lorem Ipsum</a>
+                </h4>
+                <hr>
+                <ul>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Impsum</a></li>
+                </ul>
+            </div>
+
+
+        </div>
+
+
+    </div>
+</header>
