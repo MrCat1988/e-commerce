baf9c06ca8835df83cd7564231752d62865a8bea
Mauricio Astudillo <gatoalternativo@gmail.com>
2023-05-03 18:15:29 -0500

    Commit 3-5-2023 begin header modeling

--- front-end/views/modules/header.php
+++ front-end/views/modules/header.php
@@ -1,11 +1,18 @@
-<div class="container-fluid topBar" id="top">
-    <div class="container">
-        <div class="row">
+<!-- <div class="container-fluid topBar" id="top"> -->
+
+    <!-- <div class="container"> -->
+
+
+    <div class="container-fluid">
+        <div class="row topBar">
             <div class="col-12 initial-banner">
-                <h5>10% Off, using code "ZOE10MAYO"</h5>
+                <h5>10% Off, using code "ZOEMAYO10"</h5>
             </div>
         </div>
-        <div class="row mainHeader">
+
+
+
+        <div class="row mainHeader" id="top">
             <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
                 <ul class="float-start">
                     <li>
@@ -18,70 +25,133 @@
                             <i class="bi bi-instagram socialNetwork" aria-hidden="true"></i>
                         </a>
                     </li>
+                    <li>
+                        <a href="#">
+                        <i class="bi bi-twitter socialNetwork" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="bi bi-tiktok socialNetwork" aria-hidden="true"></i>
+                        </a>
+                    </li>
                 </ul>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 register">
-                <ul>
+                <ul class="float-end">
                     <li><a href="#signinModal">Log In</a></li>
                     <li>|</li>
                     <li><a href="#loginModal">Sign In</a></li>
                 </ul>
             </div>
         </div>
+
     </div>
-</div>
+
+
+
+    <!-- </div> -->
+
+    
+<!-- </div> -->
 
 <!-- HEADER -->
 
-<header class="container-fluid">
-    <div class="container">
-        <div class="row" id="header">
-            <!-- LOGO -->
-            <div class="col-lg3 col-md-3 col-sm-2 col-xs-12" id="logo">
+<header class="container-fluid" id="header">
+    <!-- <div class="container"> -->
+
+    <!-- LOGO -->
+    <div class="row">
+        <div class="col-12">
+            <div class="d-flex justify-content-center" id="logo">
                 <a href="#">
                     <img src="http://localhost/e-commerce/back-end/views/img/template/logo.png" class="img-responsive">
                 </a>
             </div>
-            <!-- Categories -->
-            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
-                
-                <!-- Categories button -->
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategories">
-                    <p>Categories
-                        <span class="float-right">
-                            <i class="bi bi-list"></i>
-                        </span>
-                    </p>
-                </div>
-                <!-- Search -->
-                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 input-group" id="search">
-                    <input type="search" name="search" class="form-control" placeholder="Search">
-                    <span class="input-group-btn">
-                        <a href="#">
-                            <button class="btn btn-default backColor" type="submit">
-                                <i class="bi bi-search"></i>
-                            </button>
-                        </a>
-                    </span>
-                </div>
-            </div>
-            <!-- Shoping bag -->
-            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
-                <a href="#">
+
+        </div>
+
+    </div>
+
+
+
+    <div class="row">
+
+        <!-- CATEGORIES BUTTON -->
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12" id="categories">
+            <p>Categories
+                <span class="align-middle">
+                    <i class="bi bi-list"></i>
+                </span>
+            </p>
+        </div>
+
+        <!-- SEARCHING BOX -->
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 input-group w-auto">
+            <input type="search" name="search" class="form-control" placeholder="Search">
+            <!-- <span class="input-group"> -->
+                <!-- <a href="#"> -->
+                    <button class="btn btn-dark " type="submit">
+                        <i class="bi bi-search"></i>
+                    </button>
+                <!-- </a> -->
+            <!-- </span> -->
+        </div>
+
+        <!-- SHOPPING BAG -->
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <a href="#" class="d-inline">
                     <button class="btn btn-default float-left backcolor">
                         <i class="bi bi-bag" aria-hidden="true"></i>
                     </button>
                 </a>
-                <p>Shopping Bag <span class="bagQuantity"> <br> USD $ <span class="totalBag">100.00</span></span></p>
-            </div>
-
+                <p class="d-inline">Shopping Bag <span class="bagQuantity"> <br> USD $ <span class="totalBag">100.00</span></span></p>
         </div>
+
+        <!-- CATEGORIES BUTTON -->
+        <!-- <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            <p>Categories
+                <span>
+                    <i class="bi bi-list"></i>
+                </span>
+            </p>
+        </div> -->
+
+
+        <!-- SEARCHING BOX -->
+        <!-- <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 input-group">
+            <input type="search" name="search" class="form-control" placeholder="Search">
+            <span class="input-group-text">
+                <a href="#">
+                    <button class="btn btn-default " type="submit">
+                        <i class="bi bi-search"></i>
+                    </button>
+                </a>
+            </span>
+        </div> -->
+
+
+        <!-- SHOPING BAG -->
+        <!-- <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" id="carrito">
+            <a href="#" class="d-inline">
+                <button class="btn btn-default float-left backcolor">
+                    <i class="bi bi-bag" aria-hidden="true"></i>
+                </button>
+            </a>
+            <p class="d-inline">Shopping Bag <span class="bagQuantity"> <br> USD $ <span class="totalBag">100.00</span></span></p>
+        </div> -->
+
+
+    </div>
+
+
+       
 
 
 
 
         <!-- Categories -->
-        <div class="col-12 backColor" id="categories">
+        <!-- <div class="col-12 backColor" id="categories">
 
 
             <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
@@ -134,8 +204,8 @@
             </div>
 
 
-        </div>
+        </div> -->
 
 
-    </div>
+    <!-- </div> -->
 </header>
