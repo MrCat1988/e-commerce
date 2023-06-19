ac6ed84b4bc6cdcdaa00ff77d293a9f91913caa1
Mauricio Astudillo <gatoalternativo@gmail.com>
2023-06-19 18:49:48 -0500

    Fetch product info using get value

--- front-end/views/modules/product-info.php
+++ front-end/views/modules/product-info.php
@@ -1,5 +1,14 @@
 <div class="container-fluid">
     <div class="row products d-flex justify-content-evenly">
+        <?php
+            $item = 'route';
+            $value = $_GET["route"];
+            $productsRoutes = ProductsController::ctrProductInfo($item, $value);
+            echo '<pre>';
+            var_dump($productsRoutes);
+            echo '</pre>';
+
+        ?>
         <h1>Info Producto</h1>
     </div>
 </div>
