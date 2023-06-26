31d6ed2252b7038ccaa6f98088e0a84813ddbc2b
Mauricio Astudillo <gatoalternativo@gmail.com>
2023-06-26 10:24:32 -0500

    Added db data backup - Not finale

---
+++ e-commerce.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 26-06-2023 a las 17:23:18
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `e-commerce`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `category`
+--
+-- Error leyendo la estructura de la tabla e-commerce.category: #1142 - SHOW comando negado para usuario: ''@'localhost' para tabla `e-commerce`.`category`
+-- Error leyendo datos de la tabla e-commerce.category: #1064 - Algo está equivocado en su sintax cerca 'FROM `e-commerce`.`category`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+-- Error leyendo la estructura de la tabla e-commerce.products: #1142 - SHOW comando negado para usuario: ''@'localhost' para tabla `e-commerce`.`products`
+-- Error leyendo datos de la tabla e-commerce.products: #1064 - Algo está equivocado en su sintax cerca 'FROM `e-commerce`.`products`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `subcategory`
+--
+-- Error leyendo la estructura de la tabla e-commerce.subcategory: #1142 - SHOW comando negado para usuario: ''@'localhost' para tabla `e-commerce`.`subcategory`
+-- Error leyendo datos de la tabla e-commerce.subcategory: #1064 - Algo está equivocado en su sintax cerca 'FROM `e-commerce`.`subcategory`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `template`
+--
+-- Error leyendo la estructura de la tabla e-commerce.template: #1142 - SHOW comando negado para usuario: ''@'localhost' para tabla `e-commerce`.`template`
+-- Error leyendo datos de la tabla e-commerce.template: #1064 - Algo está equivocado en su sintax cerca 'FROM `e-commerce`.`template`' en la linea 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- front-end/views/js/products-load.js
+++ front-end/views/js/products-load.js
@@ -1,4 +1,15 @@
+let observer = new IntersectionObserver((entries, observer) => {
+    console.log(entries);
+},{
+    rootMargin: '0px 0px 0px 0px',
+    threshold: 1.0
+});
+
+
+
+
 $(document).ready(function(){
+    let products;
     let categoryIdRequest = $('.products').attr("categoryId");
     let subcategoryIdRequest = $('.products').attr("subCategoryId");
     let limit = '';
@@ -8,6 +19,14 @@ $(document).ready(function(){
     productsRequest.append('subcategoryIdRequest',subcategoryIdRequest);
     productsRequest.append('limit',limit);
     productsRequest.append('offset',offset);
+    loadProducts(productsRequest);
+
+
+});
+
+function loadProducts(productsRequest){
+    let productsContainer = $('.products');
+    let html = '';
     $.ajax({
             url: 'ajax/products.ajax.php',
             method: "POST",
@@ -17,10 +36,66 @@ $(document).ready(function(){
             processData: false,
             dataType: "json",
             success: function(data) {
-                console.log(data);
+                data.forEach(element => {
+                    // console.log(element);
+                    console.log(element.title);
+                    details = JSON.parse(element.details);
+                    // console.log(details);
+                    var keys = Object.keys(details);
+                    for (let i = 0; i < keys.length; i++) {
+                        var innerKeys = Object.keys(details[keys[i]]);
+                        console.log(keys[i]);
+                        // console.log(details[keys[i]]);
+                        var innerDetails = details[keys[i]];
+                        for (let x = 0; x < innerKeys.length; x++) {
+                            console.log(innerKeys[x]);
+                            console.log(innerDetails[innerKeys[x]]);
+
+                        }
+                        
+                    }
+                    // console.log(keys.length);
+
+
+
+
+                    
+            //         html += `
+            //         <div class="px-5 py-3 col-sm-6 col-lg-4">
+            //             <div class="product position-relative">
+            //                 <a href="${element.route}" idproduct="${element.idproduct}">
+            //                     <img id="product-image" class="img-fluid" src="http://localhost/e-commerce/back-end/${element.cover_img}" alt="">
+            //                 </a>
+            //                 <div class="ribbon position-absolute top-0">
+            //                     <p>${element.new == 1 ? 'NEW' :''}</p>
+            //                 </div>
+        
+            //                 <div id="sizes" class="sizes position-absolute bottom-0 px-3 w-100 d-none">
+            //                     <p class="text-center mb-0 py-1">Selecciona tu talla</p>
+            //                     <ul class="d-flex justify-content-evenly">
+            //                         <li><a class="text-decoration-none text-black" href="XS">XS</a></li>
+            //                         <li><a class="text-decoration-none text-black" href="S">S</a></li>
+            //                         <li><a class="text-decoration-none text-black" href="M">M</a></li>
+            //                         <li><a class="text-decoration-none text-black" href="L">L</a></li>
+            //                         <li><a class="text-decoration-none text-black" href="XL">XL</a></li>
+            //                     </ul>
+            //                 </div>
+            //             </div>
+            //             <div class="price-info d-flex justify-content-between">    
+            //                     <p class="description">Vestido negro</p>
+            //                     <small class="price">35.00 USD</small>
+            //                 </div>
+            //                 <div class="discount-info d-flex justify-content-end">
+            //                     <p class="discount px-2 my-0 transparent"> 0 </p>
+            //                     <p class="normal-price text-decoration-line-through my-0 transparent"> 0</p>
+            //                 </div>
+            //             </div>
+            //         </div>
+
+            //         `;
+                })
+            // productsContainer.append(html);
+
             }
     });
-
-
-
-});
+}
